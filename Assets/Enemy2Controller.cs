67175dba187b993b16557f11494917607329ea72
Anthony Reese <anthonyreesecs@yahoo.com>
2019-04-22 10:51:57 -0700

    Add basic enemy prototypes

---
+++ Assets/Enemy2Controller.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/* Controls the Enemy AI */
+
+public class Enemy2Controller : MonoBehaviour
+{
+
+    public float lookRadius = 100f;  // Detection range for player
+
+    Transform target;   // Reference to the player
+    NavMeshAgent agent; // Reference to the NavMeshAgent
+    GameObject prefab;
+    public Transform projectileSpawn;
+    float coolDown = 0f;
+
+    public Transform ProjectileSpawn
+    {
+        get
+        {
+            return projectileSpawn;
+        }
+
+        set
+        {
+            projectileSpawn = value;
+        }
+    }
+
+    void Start()
+    {
+        prefab = Resources.Load("projectile") as GameObject;
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coolDown -= Time.deltaTime;
+        // Distance to the target
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        // If inside the lookRadius, face and shoot
+        if (distance <= lookRadius)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
+            if (coolDown <= 0)
+            {
+                ShootTarget();
+                coolDown = 2f;
+            }
+            
+            // If within attacking distance
+            if (distance <= agent.stoppingDistance)
+            {
+                // Move away from the target
+  
+            }
+        }
+    }
+
+    // Rotate to face the target
+    void ShootTarget()
+    {
+        GameObject projectileInstance;
+        projectileInstance = Instantiate(prefab, projectileSpawn.position, projectileSpawn.rotation);
+        Rigidbody rb = projectileInstance.GetComponent<Rigidbody>();
+        rb.AddForce(projectileSpawn.forward * 2000);
+    }
+
+    // Show the lookRadius in editor
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
