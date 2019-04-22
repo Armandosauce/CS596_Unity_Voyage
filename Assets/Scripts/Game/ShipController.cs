ec47a4c48b12f2f281a62c931567b849e51e2462
Irina Brigido Chan <irinabchan@gmail.com>
2019-04-22 13:44:22 -0700

    Add basic collect and deliver ship parts functionality

---
+++ Assets/Scripts/Game/ShipController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public int missingParts = 1;
+    public bool isComplete = false;
+
+    public GameObject missingPart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Consider making it a trigger instead of using RigidBody for collision
+        if (collision.gameObject.name == "PlayerCharacter")
+        {
+            if (missingPart.GetComponent<ShipPartsController>().collected == true)
+            {
+                missingParts--;
+                Destroy(missingPart);
+                Debug.Log("Missing part delivered.");
+            }
+
+            if (missingParts == 0)
+            {
+                isComplete = true;
+                Debug.Log("Ship is complete!");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/ShipPartsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPartsController : MonoBehaviour
+{
+    public bool collected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "PlayerCharacter")
+        {
+            collected = true;
+            transform.gameObject.SetActive(false);
+            Debug.Log("Missing part collected.");
+        }
+    }
+}
