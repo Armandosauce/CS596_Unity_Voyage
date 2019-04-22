d075f7f4c376aa70ac7047173c1b8a0e20c08204
Irina Brigido Chan <irinabchan@gmail.com>
2019-04-22 16:55:06 -0700

    Instantiate ship parts at random position from array of spawn points

--- Assets/Scripts/Game/ShipController.cs
+++ Assets/Scripts/Game/ShipController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    public int missingParts = 1;
-    public bool isComplete = false;
+    public int missingParts;
+    public bool isComplete;
+    public GameObject[] missingPartsArray;
+    public Transform[] spawnPoints;
+    private List<GameObject> activeParts = new List<GameObject>();
 
-    public GameObject missingPart;
+    private void Awake()
+    {
+        isComplete = false;
+        missingParts = missingPartsArray.Length;
+        SpawnShipParts();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,11 +34,14 @@ public class ShipController : MonoBehaviour
         // Consider making it a trigger instead of using RigidBody for collision
         if (collision.gameObject.name == "PlayerCharacter")
         {
-            if (missingPart.GetComponent<ShipPartsController>().collected == true)
+            missingParts = 0;
+
+            foreach (GameObject part in activeParts)
             {
-                missingParts--;
-                Destroy(missingPart);
-                Debug.Log("Missing part delivered.");
+                if (part.GetComponent<ShipPartsController>().collected == false)
+                {
+                    missingParts++;
+                }
             }
 
             if (missingParts == 0)
@@ -38,6 +49,25 @@ public class ShipController : MonoBehaviour
                 isComplete = true;
                 Debug.Log("Ship is complete!");
             }
+            else
+            {
+                Debug.Log("Number of parts missing: " + missingParts);
+            }
+        }
+    }
+
+    public void SpawnShipParts()
+    {
+        Debug.Log("Spawning ship parts on map.");
+        List<Transform> activeSpawnPoints = new List<Transform>(spawnPoints);
+
+
+        foreach (GameObject part in missingPartsArray)
+        {
+            int pos = Random.Range(0, activeSpawnPoints.Count);
+            GameObject prefab = Instantiate(part, activeSpawnPoints[pos]);
+            activeParts.Add(prefab);
+            activeSpawnPoints.RemoveAt(pos);
         }
     }
 }
