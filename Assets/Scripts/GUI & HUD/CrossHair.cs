0d9dbc4a77b7d294036f78a575ba41aa8ddff957
Armando Perez <armandoperez0001@gmail.com>
2019-04-27 17:39:36 -0700

    added crosshair, will work on transparency

--- Assets/Scripts/GUI & HUD/CrossHair.cs
+++ Assets/Scripts/GUI & HUD/CrossHair.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class CrossHair : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Texture2D image;
+    [Range(0, 1)]
+    public float yOffsetPercentage;
+    [Range(0, 1)]
+    public float imageAlpha;
+
+    private float offsetY;
+    private float prevAlpha;
+
+    private void Awake()
     {
-        
+        prevAlpha = imageAlpha;
+        image.SetPixels(modifyAlpha(image.GetPixels(), imageAlpha));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
+        if(imageAlpha != prevAlpha)
+        {
+            prevAlpha = imageAlpha;
+            Debug.Log("CHANGED ALPHA");
+            image.SetPixels(modifyAlpha(image.GetPixels(), imageAlpha));
+        }
+
+        float xMin = (Screen.width / 2) - (image.width / 2);
+        float yMin = (Screen.height / 2) - (image.height / 2);
+        offsetY = yMin * yOffsetPercentage;
+        yMin = yMin - offsetY;
         
+        GUI.DrawTexture(new Rect(xMin, yMin, image.width, image.height), image);
     }
+
+    private Color[] modifyAlpha(Color[] colors, float alpha)
+    {
+        for(int i = 0; i < colors.Length; i++)
+        {
+            colors[i].a = alpha;
+        }
+        return colors;
+    }
+    
 }
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour {
         {
             Destroy(this.gameObject);
         }
-        
+        Cursor.visible = false;
     }
     
 }
