01e1e77ae8d76cd829a4eb2e208a742f5bf1b8df
Irina Brigido Chan <irinabchan@gmail.com>
2019-05-04 17:39:58 -0700

    Create a health powerup that increments the player's health when collected

---
+++ Assets/Scripts/Game/powerUpItemsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerUpItemsController : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip itemCollectedSound;
+    GameObject player;
+    public float healthPickup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GameObject.Find("AudioSource").GetComponent<AudioSource>();
+        player = GameObject.Find("PlayerCharacter");
+        healthPickup = 10;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "PlayerCharacter")
+        {
+            audioSource.PlayOneShot(itemCollectedSound, 1f);
+            if (player.gameObject.GetComponent<Player>().currentHealth < player.gameObject.GetComponent<Player>().health)
+            {
+                player.gameObject.GetComponent<Player>().currentHealth += healthPickup;
+            }
+            Destroy(this.gameObject);
+            Debug.Log("Health pack collected.");
+        }
+    }
+}
