b5130d81ba1854ce43e32a1a0d6f0227f3a16da8
Irina Brigido Chan <irinabchan@gmail.com>
2019-05-04 18:02:21 -0700

    Health powerups replenish a random value from a specified interval instead of a constant number; Add new sound for health powerup

--- Assets/Scripts/Game/powerUpItemsController.cs
+++ Assets/Scripts/Game/powerUpItemsController.cs
@@ -7,14 +7,13 @@ public class powerUpItemsController : MonoBehaviour
     AudioSource audioSource;
     public AudioClip itemCollectedSound;
     GameObject player;
-    public float healthPickup;
+    public int healthPickup;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GameObject.Find("AudioSource").GetComponent<AudioSource>();
         player = GameObject.Find("PlayerCharacter");
-        healthPickup = 10;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -22,12 +21,24 @@ public class powerUpItemsController : MonoBehaviour
         if (collision.gameObject.name == "PlayerCharacter")
         {
             audioSource.PlayOneShot(itemCollectedSound, 1f);
-            if (player.gameObject.GetComponent<Player>().currentHealth < player.gameObject.GetComponent<Player>().health)
+            float currentHealth = player.gameObject.GetComponent<Player>().currentHealth;
+            float maxHealth = player.gameObject.GetComponent<Player>().health;
+
+            if (currentHealth < maxHealth)
             {
-                player.gameObject.GetComponent<Player>().currentHealth += healthPickup;
+                // Generate a random value for the health pack and make sure the player doesn't go above max health
+                healthPickup = Random.Range(5, 20);
+                if((currentHealth + healthPickup) <= maxHealth)
+                {
+                    player.gameObject.GetComponent<Player>().currentHealth += healthPickup;
+                    Debug.Log("Health restored: " + healthPickup);
+                }
+                else
+                {
+                    player.gameObject.GetComponent<Player>().currentHealth = maxHealth;
+                }
             }
             Destroy(this.gameObject);
-            Debug.Log("Health pack collected.");
         }
     }
 }
