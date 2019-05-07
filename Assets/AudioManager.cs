978135fd17c9eae1f40f6b7b7c87abd0f43bccff
Armando Perez <armandoperez0001@gmail.com>
2019-05-06 21:46:43 -0700

    fixed pickup scripts and added AudioManager singleton component to AudioSource.

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [HideInInspector]
+    public AudioSource source;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        source = GetComponent<AudioSource>();
+    }
+}
--- Assets/Scripts/Game/ShipPartsController.cs
+++ Assets/Scripts/Game/ShipPartsController.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 public class ShipPartsController : MonoBehaviour
 {
     public bool collected = false;
-
-    AudioSource audioSource;
+    
     public AudioClip itemCollectedSound;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerStay(Collider other)
     {
-        audioSource = GameObject.Find("AudioSource").GetComponent<AudioSource>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.name == "PlayerCharacter")
+        if (other.gameObject.tag == "Player")
         {
-            audioSource.PlayOneShot(itemCollectedSound, 1f);
-
-            collected = true;
-            transform.gameObject.SetActive(false);
-            Debug.Log("Missing part collected.");
+            collectPart();
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            collectPart();
+        }
+    }
+    
+    private void collectPart()
+    {
+        AudioManager.instance.source.PlayOneShot(itemCollectedSound, 1f);
+
+        collected = true;
+        transform.gameObject.SetActive(false);
+        Debug.Log("Missing part collected.");
     }
 }
